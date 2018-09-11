feedf11094ade743eb04041760564a5c6243de13
anythingdevops <43129357+anythingdevops@users.noreply.github.com>
2018-09-11 23:07:22 +0530

    Update uttest.js

--- test/unit/uttest.js
+++ test/unit/uttest.js
@@ -8,72 +8,9 @@ describe('Array', function() {
     });
 });
 
-describe('random function', () => {
-    let getUniqRandGenNums = numOfPars => {
-        let randomsNumbers = [];
-        let params = [[], [5], [1, 10]][numOfPars];
-        for (var i = 0; i < 10000; i++) {
-            randomsNumbers.push(random(...params));
-        }
-        randomsNumbers = [...new Set(randomsNumbers)];
-        let minRandom = min(randomsNumbers); // I implemented min
-        let maxRandom = max(randomsNumbers); // I implemented max
-        return {
-            randomsNumbers,
-            minRandom,
-            maxRandom
-        }
-    }
-    describe('no patameters passed', () => {
-        let result = getUniqRandGenNums(0);
-        let minRandom = result.minRandom;
-        let maxRandom = result.maxRandom;
-        let randomsNumbers = result.randomsNumbers;
-        it('should generate different numbers', () => {
-            expect(randomsNumbers.length).to.be.above(1);
-        });
-        it('should not generate a number below the lower bound', () => {
-            expect(minRandom).to.be.within(0, 1);
-        });
-        it('should not generate a number above the upper bound', () => {
-            expect(maxRandom).to.be.within(0, 1);
-        });
-    });
-    describe('one patameter passed', () => {
-        let result = getUniqRandGenNums(1);
-        let minRandom = result.minRandom;
-        let maxRandom = result.maxRandom;
-        let randomsNumbers = result.randomsNumbers;
-        it('should generate different numbers', () => {
-            expect(randomsNumbers.length).to.be.above(1);
-        });
-        it('should not generate a number below the lower bound', () => {
-            expect(minRandom).to.be.within(0, 5);
-        });
-        it('should not generate a number above the upper bound', () => {
-            expect(maxRandom).to.be.within(0, 5);
-        });
-    });
-    describe('two patameters passed', () => {
-        let result = getUniqRandGenNums(2);
-        let minRandom = result.minRandom;
-        let maxRandom = result.maxRandom;
-        let randomsNumbers = result.randomsNumbers;
-        it('should generate different numbers', () => {
-            expect(randomsNumbers.length).to.be.above(1);
-        });
-        it('should not generate a number below the lower bound', () => {
-            expect(minRandom).to.be.within(1, 10);
-        });
-        it('should not generate a number above the upper bound', () => {
-            expect(maxRandom).to.be.within(1, 10);
-        });
-    });
-});
-
 describe('Equality', function() {
     it('should be equal to 10', function() {
-        var num = 10;
+        var num = 110;
         expect(num).to.be.eq(10);
     })});
     
